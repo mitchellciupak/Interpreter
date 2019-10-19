ee0806cb8398efd6bcffb64d68c7f4a574644954
Peter Mandy <laggant@gmail.com>
2019-10-19 11:54:50 -0400

    Bootstrap

---
+++ cmake-build-debug/CMakeFiles/HW8.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mitchellciupak/Documents/ECE30862/HW8/Alarm.cpp" "/Users/mitchellciupak/Documents/ECE30862/HW8/cmake-build-debug/CMakeFiles/HW8.dir/Alarm.cpp.o"
+  "/Users/mitchellciupak/Documents/ECE30862/HW8/GPS.cpp" "/Users/mitchellciupak/Documents/ECE30862/HW8/cmake-build-debug/CMakeFiles/HW8.dir/GPS.cpp.o"
+  "/Users/mitchellciupak/Documents/ECE30862/HW8/Map.cpp" "/Users/mitchellciupak/Documents/ECE30862/HW8/cmake-build-debug/CMakeFiles/HW8.dir/Map.cpp.o"
+  "/Users/mitchellciupak/Documents/ECE30862/HW8/Observer.cpp" "/Users/mitchellciupak/Documents/ECE30862/HW8/cmake-build-debug/CMakeFiles/HW8.dir/Observer.cpp.o"
+  "/Users/mitchellciupak/Documents/ECE30862/HW8/Subject.cpp" "/Users/mitchellciupak/Documents/ECE30862/HW8/cmake-build-debug/CMakeFiles/HW8.dir/Subject.cpp.o"
+  "/Users/mitchellciupak/Documents/ECE30862/HW8/main.cpp" "/Users/mitchellciupak/Documents/ECE30862/HW8/cmake-build-debug/CMakeFiles/HW8.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/HW8.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW8.dir/Alarm.cpp.o"
+  "CMakeFiles/HW8.dir/GPS.cpp.o"
+  "CMakeFiles/HW8.dir/main.cpp.o"
+  "CMakeFiles/HW8.dir/Map.cpp.o"
+  "CMakeFiles/HW8.dir/Observer.cpp.o"
+  "CMakeFiles/HW8.dir/Subject.cpp.o"
+  "HW8.pdb"
+  "HW8"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
