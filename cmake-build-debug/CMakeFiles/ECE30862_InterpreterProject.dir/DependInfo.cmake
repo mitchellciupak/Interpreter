5433fe7068fcb2ce0c036472920ad31015b73f71
Mitchell Ciupak <laggant@gmail.com>
2019-10-27 20:28:20 -0400

    Ignore

---
+++ cmake-build-debug/CMakeFiles/ECE30862_InterpreterProject.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mitchellciupak/Documents/ECE30862-InterpreterProject/ByteCode.cpp" "/Users/mitchellciupak/Documents/ECE30862-InterpreterProject/cmake-build-debug/CMakeFiles/ECE30862_InterpreterProject.dir/ByteCode.cpp.o"
+  "/Users/mitchellciupak/Documents/ECE30862-InterpreterProject/MemoryObject.cpp" "/Users/mitchellciupak/Documents/ECE30862-InterpreterProject/cmake-build-debug/CMakeFiles/ECE30862_InterpreterProject.dir/MemoryObject.cpp.o"
+  "/Users/mitchellciupak/Documents/ECE30862-InterpreterProject/Value.cpp" "/Users/mitchellciupak/Documents/ECE30862-InterpreterProject/cmake-build-debug/CMakeFiles/ECE30862_InterpreterProject.dir/Value.cpp.o"
+  "/Users/mitchellciupak/Documents/ECE30862-InterpreterProject/datastructs/Stack.cpp" "/Users/mitchellciupak/Documents/ECE30862-InterpreterProject/cmake-build-debug/CMakeFiles/ECE30862_InterpreterProject.dir/datastructs/Stack.cpp.o"
+  "/Users/mitchellciupak/Documents/ECE30862-InterpreterProject/main.cpp" "/Users/mitchellciupak/Documents/ECE30862-InterpreterProject/cmake-build-debug/CMakeFiles/ECE30862_InterpreterProject.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../codes"
+  "../datastructs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ECE30862_InterpreterProject.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ECE30862_InterpreterProject.dir/datastructs/Stack.cpp.o"
+  "CMakeFiles/ECE30862_InterpreterProject.dir/ByteCode.cpp.o"
+  "CMakeFiles/ECE30862_InterpreterProject.dir/main.cpp.o"
+  "CMakeFiles/ECE30862_InterpreterProject.dir/MemoryObject.cpp.o"
+  "CMakeFiles/ECE30862_InterpreterProject.dir/Value.cpp.o"
+  "ECE30862_InterpreterProject.pdb"
+  "ECE30862_InterpreterProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ECE30862_InterpreterProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
