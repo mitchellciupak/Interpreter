695f66e499cbb33df7c011d5d52e57a422c747cf
Mitchell Ciupak <laggant@gmail.com>
2019-10-30 15:42:17 -0400

    Auto stash before merge of "feature_Value" and "origin/feature_Value"

--- datastructs/Value.cpp
+++ datastructs/Value.cpp
@@ -1,1 +1,20 @@
 #include "Value.h"
+<<<<<<< Updated upstream
+=======
+
+char Value::getChar() {
+    return this->cVal;
+}
+
+short Value::getShort() {
+    return this->sVal;
+}
+
+int Value::getInt() {
+    return this->iVal;
+}
+
+float Value::getFloat() {
+    return this->fVal;
+}
+>>>>>>> Stashed changes
