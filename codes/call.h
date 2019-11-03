085d268e892c2e860e720a06fc2c17cd319a0d2c
Mitchell Ciupak <laggant@gmail.com>
2019-11-03 16:54:12 -0500

    Untagged TODO

--- codes/call.h
+++ codes/call.h
@@ -6,14 +6,14 @@
 #include "ByteCode.h"
 #include "FrameStack.h"
 
-class call : public ByteCode{ //TODO FIX
+class call : public ByteCode{
     public:
         static void execute(){
             FrameStack::fpsp += 1;
             int i = Stack::sp - Stack::stackVect[Stack::sp].iVal - 1;
             FrameStack::FrameVect.push_back(i);
             Stack::sp--;
-            Buffer::pc = Stack::stackVect[Stack::sp--].iVal;
+            Buffer::pc = Stack::stackVect[Stack::sp--].iVal - 1;
         };
 };
 
